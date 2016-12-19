3626e2a0a73381e97e06edbe64e8e1778d5d9d7f
Brunno Vodola <bvodola@gmail.com>
2016-12-19 14:24:06 -0200

    Removed react-tap-event-plugin

--- client/main.jsx
+++ client/main.jsx
@@ -6,7 +6,7 @@ import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from '../imports/ui/App.jsx';
 
 // Frontend Libraries Imports
@@ -18,7 +18,7 @@ Meteor.startup(() => {
 
 	// Needed for onTouchTap
 	// http://stackoverflow.com/a/34015469/988941
-	injectTapEventPlugin();
+	// injectTapEventPlugin();
 
 	// Locale configurations for the client (from the moment.js module)
 	moment.locale('pt-br');
