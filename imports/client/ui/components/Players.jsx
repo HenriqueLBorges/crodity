22c82c1fa036c8cb1fab48f4324c4a31fb2d4251
tgremi <thallesgremi@gmail.com>
2017-02-15 12:21:59 -0200

    a

---
+++ imports/client/ui/components/Players.jsx
@@ -0,0 +1,22 @@
+import { Meteor } from 'meteor/meteor';
+import React, { Component } from 'react';
+import { Helpers } from '../helpers/Helpers.jsx';
+import ReactDOM from 'react-dom';
+
+class Players extends Component {
+
+searchVideoYoutube(){
+  
+}
+
+  render() {
+
+    return (
+      <div className="player">
+       Teste
+      </div>
+    );
+
+  }
+}
+export default Players;
